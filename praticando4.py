ee904b749cebd5ab5ad152c06967284a7ea8a67f
GabrielJulioSantos <145797553+GabrielJulioSantos@users.noreply.github.com>
2023-10-10 10:38:27 -0300

    praticando-final
    hehehe

--- praticando4.py
+++ praticando4.py
@@ -1,4 +1,6 @@
 n=int(input("digite um numero"))
 a=n+1,n+2,n+3,n+4,n+5,n+6,n+7,n+8,n+9,n+10
 s=n-1,n-2,n-3,n-4,n-5,n-6,n-7,n-8,n-9,n-10
-u=n*1,n*2,n*3,n*4,n*5,n*6,n*7,n*
+u=n*1,n*2,n*3,n*4,n*5,n*6,n*7,n*8,n*9,n*10
+t=n/1,n/2,n/3,n/4,n/5,n/6,n/7,n/8,n/9,n/10
+print("de acordo com a analise do seu numero{},a adiçao é{},a subtraçao é{},a mutipliacaçao é{},e a divisao é{}".format(n,a,s,u,t))
---
+++ praticando5.py
@@ -0,0 +1,3 @@
+n=float(input("digite quanto vc tem na carteira"))
+d=n/5,01
+print("de acordo com a oq vc tem carteira{},vc tera {}de dolar".format(n,d))
